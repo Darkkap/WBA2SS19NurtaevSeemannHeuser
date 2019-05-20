8ddd3fc78a67519baf7abfe2b03e15538ad79798
Exokay <t.seemann@wsb-cnc.de>
2019-05-20 13:17:18 +0200

    Beispiel für Funktionen & Rest EndPoints

--- Dienstgeber/app.js
+++ Dienstgeber/app.js
@@ -1,19 +1,26 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var app = express();
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+
+
+let function2 = require('./routes/function2');
+let usersRouter = require('./routes/users');
+let function1 = require('./routes/function1');
+let app = express();
 app.io=0;
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+//Routen Definierung
+
+app.use('/function1', function1);
+app.use('/function2', function2);
 app.use('/users', usersRouter);
 
-
+//Routen Definierung Ende
 module.exports = app;
