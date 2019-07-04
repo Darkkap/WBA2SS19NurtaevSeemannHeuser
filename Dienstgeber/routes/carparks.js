9c4fe265fc29e499249f13430e5a5bcb6542405c
Exokay <tobias.seemann@hotmail.com>
2019-07-04 10:48:30 +0200

    added carparks live data

--- Dienstgeber/routes/carparks.js
+++ Dienstgeber/routes/carparks.js
@@ -2,129 +2,150 @@ let express = require('express');
 let router = express.Router();
 let mysql = require('mysql');
 let connection = mysql.createConnection({
-  host: 'exo.ovh',
-  user: 'wbauser',
-  password: 'wba-user',
-  database: 'wba_data'
+    host: 'exo.ovh',
+    user: 'wbauser',
+    password: 'wba-user',
+    database: 'wba_data'
 });
 connection.connect();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {  // Alle Parkhäuser
-  let parkhausobj = {};
-  connection.query("Select * from parkhaus_info" , function (error, results, fields) {
-    parkhausobj.parkhaus = [];
-    for (let i = 0;i <= results.length-1;i++) {
-      parkhausobj.parkhaus.push(results[i]);
-    }
-    if (error) {
-      res.status(500).json({"parkhaus":"Error"});
-    }  else {
-      res.status(200).json(parkhausobj);
-      console.log(parkhausobj);
-    }
-    next();
-    res.end();
-  });
+
+router.get('/live', function (req, res, next) {  // Alle Parkhäuser
+    let parkhausobj = {};
+    connection.query("Select * from parkhaus_info where winkel > 1", function (error, results, fields) {
+        parkhausobj.parkhaus = [];
+        for (let i = 0; i <= results.length - 1; i++) {   // Loop durch die Info
+            parkhausobj.parkhaus.push(results[i]);
+            delete parkhausobj.parkhaus[parkhausobj.parkhaus.length - 1].id; // Live Daten für jedes Parkhaus holen.
+            connection.query("select frei,gesamt,status from parkhaus_data where lfdnr='" + parkhausobj.parkhaus[parkhausobj.parkhaus.length - 1].parkhaus_id + "' order by id desc limit 1", function (error, result, fields) {
+                parkhausobj.parkhaus[i].live = result[0]; // Anhängen an das Parkhausobj.
+                if (i === results.length - 1) {
+                    res.status(200).json(parkhausobj);
+                    next();
+                    res.end();
+                }
+            });
+        }
+    });
 });
 
-router.post('/', function(req, res, next) { // Anlegen eines neuen Parkhaus
+/* GET home page. */
+router.get('/', function (req, res, next) {  // Alle Parkhäuser
+    let parkhausobj = {};
+    connection.query("Select * from parkhaus_info where winkel > 1", function (error, results, fields) {
+        parkhausobj.parkhaus = [];
+        if (error) {
+            res.status(500).json({"parkhaus": "Error"});
+        } else {
+            for (let i = 0; i <= results.length - 1; i++) {
+                parkhausobj.parkhaus.push(results[i]);
+            }
+            console.log(parkhausobj.parkhaus);
 
-  let position = 0;
-  if(req.body.position !== undefined) {
-    position = req.body.position;
-  }
-  let parkhaus_id =0;
-  if(req.body.parkhaus_id !== undefined) {
-    parkhaus_id = req.body.parkhaus_id;
-  }
-  let winkel = 0;
-  if(req.body.winkel !== undefined) {
-    winkel = req.body.winkel;
-  }
-
-
-
-
-  connection.query("Insert into  parkhaus_info values ('','"+position+"','"+parkhaus_id+"','"+winkel+"')", function (error, results, fields) {
-    if (error) {
-      res.status(404).json({"Parkhaus": "Error. Fehler beim anlegen"});
-      next();
-      res.end();
-    } else {
-      connection.query("SELECT LAST_INSERT_ID() as id", function (error, results, fields) {
-        res.status(200).json(results);
+            res.status(200).json(parkhausobj);
+            console.log(parkhausobj);
+        }
         next();
         res.end();
-      });
-    }
-  });
+    });
 });
 
-router.get('/:id', function(req, res, next) {//Abruf Parkhaus ID
-  let id = req.params.id;
-  id = id.replace(":", "");
-  let sucheobj = {};
-  connection.query("Select * from parkhaus_info where parkhaus_id='" + id + "'", function (error, results, fields) {
-    if (results.length < 1) {
-      res.status(404).json({"Parkhaus_info": "Error. Keine Details gefunden"});
-      next();
-      res.end();
-    } else {
-      sucheobj.suche = results;
-      res.status(200).json(sucheobj);
-      next();
-      res.end();
+
+router.post('/', function (req, res, next) { // Anlegen eines neuen Parkhaus
+
+    let position = 0;
+    if (req.body.position !== undefined) {
+        position = req.body.position;
     }
-  });
+    let parkhaus_id = 0;
+    if (req.body.parkhaus_id !== undefined) {
+        parkhaus_id = req.body.parkhaus_id;
+    }
+    let winkel = 0;
+    if (req.body.winkel !== undefined) {
+        winkel = req.body.winkel;
+    }
+
+
+    connection.query("Insert into  parkhaus_info values ('','" + position + "','" + parkhaus_id + "','" + winkel + "')", function (error, results, fields) {
+        if (error) {
+            res.status(404).json({"Parkhaus": "Error. Fehler beim anlegen"});
+            next();
+            res.end();
+        } else {
+            connection.query("SELECT LAST_INSERT_ID() as id", function (error, results, fields) {
+                res.status(200).json(results);
+                next();
+                res.end();
+            });
+        }
+    });
 });
 
-router.put('/:id', function(req, res, next) { //Bearbeiten des Parkhaus X
-
-  /*
-
-    {
-            "position": "50.7319997,7.0956021,17.25z",
-            "parkhaus_id": 1,
-            "winkel": 195
-    }
-
-  **/
-  let id = req.params.id;
-  id = id.replace(":", "");
-  let position = req.body.position;
-  let parkhaus_id =req.body.parkhaus_id;
-  let winkel = req.body.winkel;
-  if(position !== undefined) {
-    connection.query("update parkhaus_info set position='"+position+"'  where id='" + id + "'", function (error, results, fields) {
+router.get('/id:id', function (req, res, next) {//Abruf Parkhaus ID
+    let id = req.params.id;
+    id = id.replace(":", "");
+    let sucheobj = {};
+    connection.query("Select * from parkhaus_info where parkhaus_id='" + id + "'", function (error, results, fields) {
+        if (results.length < 1) {
+            res.status(404).json({"Parkhaus_info": "Error. Keine Details gefunden"});
+            next();
+            res.end();
+        } else {
+            sucheobj.suche = results;
+            res.status(200).json(sucheobj);
+            next();
+            res.end();
+        }
     });
-  }
-  if(parkhaus_id !== undefined) {
-    connection.query("update parkhaus_info set parkhaus_id='"+parkhaus_id+"'  where id='" + id + "'", function (error, results, fields) {
-    });
-  }
-  if(winkel !== undefined) {
-    connection.query("update parkhaus_info set winkel='"+winkel+"'  where id='" + id + "'", function (error, results, fields) {
-    });
-  }
-  res.status(200).write("Änderung abgeschlossen.");
-  next();
-  res.end();
-
 });
 
-router.delete('/:id', function(req, res, next) {  // Löschen des Parkhaus X
-  let id = req.params.id;
-  id = id.replace(":", "");
-  connection.query("delete from parkhaus_info where parkhaus_id ='" + id + "'", function (error, results, fields) {
-    if (error) {
-      res.status(500).write("Internal Error");
-    } else {
-      res.status(200).write("OK, ID: " + id + " gelöscht.");
+router.put('/:id', function (req, res, next) { //Bearbeiten des Parkhaus X
+
+    /*
+
+      {
+              "position": "50.7319997,7.0956021,17.25z",
+              "parkhaus_id": 1,
+              "winkel": 195
+      }
+
+    **/
+    let id = req.params.id;
+    id = id.replace(":", "");
+    let position = req.body.position;
+    let parkhaus_id = req.body.parkhaus_id;
+    let winkel = req.body.winkel;
+    if (position !== undefined) {
+        connection.query("update parkhaus_info set position='" + position + "'  where id='" + id + "'", function (error, results, fields) {
+        });
     }
+    if (parkhaus_id !== undefined) {
+        connection.query("update parkhaus_info set parkhaus_id='" + parkhaus_id + "'  where id='" + id + "'", function (error, results, fields) {
+        });
+    }
+    if (winkel !== undefined) {
+        connection.query("update parkhaus_info set winkel='" + winkel + "'  where id='" + id + "'", function (error, results, fields) {
+        });
+    }
+    res.status(200).write("Änderung abgeschlossen.");
     next();
     res.end();
-  });
+
+});
+
+router.delete('/:id', function (req, res, next) {  // Löschen des Parkhaus X
+    let id = req.params.id;
+    id = id.replace(":", "");
+    connection.query("delete from parkhaus_info where parkhaus_id ='" + id + "'", function (error, results, fields) {
+        if (error) {
+            res.status(500).write("Internal Error");
+        } else {
+            res.status(200).write("OK, ID: " + id + " gelöscht.");
+        }
+        next();
+        res.end();
+    });
 });
 
 module.exports = router;
